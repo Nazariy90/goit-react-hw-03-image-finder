068178fcbbbe5c9060c8023d6b460c2805460247
Nazariy90 <pinhvin123@gmail.com>
2023-05-02 23:05:44 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,103 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import ImageGallery from './imageGallery/ImageGallery';
+import Searchbar from './searchbar/Searchbar';
+import { getImages, PER_PAGE } from '../network/api';
+import Button from './button/Button';
+import Loader from './loader/Loader';
+import css from './App.module.css';
+
+export class App extends Component {
+  static propTypes = {
+    loading: PropTypes.bool,
+    page: PropTypes.number,
+    searchValue: PropTypes.string,
+    hits: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        webformatURL: PropTypes.string.isRequired,
+        tags: PropTypes.string.isRequired,
+      })
+    ),
+    totalHits: PropTypes.number,
+  };
+
+  state = {
+    loading: false,
+    page: 1,
+    searchValue: '',
+    hits: [],
+    totalHits: 0,
+  };
+
+  handleLoadMore = async () => {
+    this.setState({ loading: true });
+
+    try {
+      const images = await getImages({
+        page: this.state.page + 1,
+        searchValue: this.state.searchValue,
+      });
+
+      this.setState(prev => {
+        return {
+          page: prev.page + 1,
+          hits: [...prev.hits, ...images.hits],
+          totalHits: images.totalHits,
+        };
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  handleSearch = async value => {
+    if (!value) {
+      this.setState({
+        searchValue: value,
+        hits: [],
+        totalHits: 0,
+      });
+    } else {
+      this.setState({ loading: true });
+
+      try {
+        const images = await getImages({
+          page: 1,
+          searchValue: value,
+        });
+        this.setState({
+          page: 1,
+          searchValue: value,
+          hits: images.hits,
+          totalHits: images.totalHits,
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.setState({ loading: false });
+      }
+    }
+  };
+
+  render() {
+    const hasMoreImages =
+      this.state.totalHits > 0 &&
+      this.state.page * PER_PAGE < this.state.totalHits;
+
+    return (
+      <div className={css.AppContainer}>
+        <Searchbar onSearch={this.handleSearch} />
+        <ImageGallery hits={this.state.hits} />
+
+        {this.state.loading && <Loader />}
+
+        {hasMoreImages && !this.state.loading && (
+          <Button onloadMore={this.handleLoadMore} />
+        )}
+      </div>
+    );
+  }
+}
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -1,1 +1,44 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
 import css from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    input: '',
+  };
+
+  render() {
+    const { onSearch } = this.props;
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      onSearch(this.state.input);
+    };
+
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={handleSubmit}>
+          <button type="submit" className={css.SearchFormButton}>
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.input}
+            onChange={e => this.setState({ input: e.target.value })}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
+
+export default Searchbar;
